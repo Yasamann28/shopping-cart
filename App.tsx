6ffe8150928210b739a44b7b3200685c3ec1ad5a
Yasamann28 <2ujasnm@gmail.com>
2022-08-25 15:04:25 +0430

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import About from './pages/About';
+import Store from './pages/Store';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </div>
+  );
+}
