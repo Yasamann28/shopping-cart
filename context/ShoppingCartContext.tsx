1e619ea20e7598f279a3239d10d9eee7882ddc60
Yasamann28 <2ujasnm@gmail.com>
2022-08-26 14:09:27 +0430

    1 file modified

--- context/ShoppingCartContext.tsx
+++ context/ShoppingCartContext.tsx
@@ -23,8 +23,29 @@ function useShoppingCart() {
 function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+  function increaseCartQuantity(id: number) {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{ getItemQuantity, increaseCartQuantity }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
