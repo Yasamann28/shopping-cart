c9b6340d7440154f5087c756e85a7e78e59591c6
Yasamann28 <2ujasnm@gmail.com>
2022-08-26 12:23:49 +0430

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,18 +5,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store';
 import Navbar from './components/Navbar';
+import ShoppingCartProvider from './context/ShoppingCartContext';
 
 export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </div>
+    <ShoppingCartProvider>
+      <div>
+        <Navbar />
+        <Container className="mb-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+      </div>
+    </ShoppingCartProvider>
   );
 }
