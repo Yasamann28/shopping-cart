9fb661a059e6ec286ccaab284084bb18b1b68b5b
Yasamann28 <2ujasnm@gmail.com>
2022-08-26 12:33:18 +0430

    1 file modified

--- context/ShoppingCartContext.tsx
+++ context/ShoppingCartContext.tsx
@@ -1,14 +1,14 @@
 import React, { createContext, ReactNode, useContext } from 'react';
 
-const ShoppingCartContext = createContext({});
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
 
+const ShoppingCartContext = createContext({});
 export default function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
-type ShoppingCartProviderProps = {
-  children: ReactNode;
-};
 export default function ShoppingCartProvider({
   children,
 }: ShoppingCartProviderProps) {
