95ce951687b31cc72386a6e03b28f1d26e1597eb
Yasamann28 <2ujasnm@gmail.com>
2022-08-27 12:27:43 +0430

    1 file modified, 1 added

---
+++ components/ShoppingCart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Offcanvas } from 'react-bootstrap';
+
+export function ShoppingCart() {
+  return (
+    <Offcanvas show={true} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+    </Offcanvas>
+  );
+}
