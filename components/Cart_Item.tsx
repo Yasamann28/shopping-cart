eacddff6d503718735ff482347ede520c0272e08
Yasamann28 <2ujasnm@gmail.com>
2022-08-27 13:50:11 +0430

    1 file modified

--- components/Cart_Item.tsx
+++ components/Cart_Item.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Stack } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import storeItems from '../data/items.json';
+import formatCurrency from '../utilities/formatCurrency';
 
 type Cart_Item_Props = {
   id: number;
   quantity: number;
 };
-export function Cart_Item({ id, quantity }: Cart_Item_Props) {}
+
+export function Cart_Item({ id, quantity }: Cart_Item_Props) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: '125px', height: '75px', objectFit: 'cover' }}
+      />
+      <div className="me-auto">
+        <div>
+          <span style={{ fontSize: '1.1rem ' }}>{item.name}</span>
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: '0.75rem' }}>
+              {''} x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: '0.8rem' }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+    </Stack>
+  );
+}
